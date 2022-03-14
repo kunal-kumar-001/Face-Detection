59ee6d4c609d5f5071d8d058460f880d3237d7c5
kunal-kumar-001 <kumarkunal1849@gmail.com>
2022-03-14 12:21:01 +0530

    Initial commits

---
+++ MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.mlkunal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.mlkunal.helpers.ImageHelperActivity;
+import com.example.mlkunal.image.FaceDetectionActivity;
+import com.example.mlkunal.image.FlowerIdentificationActivity;
+import com.example.mlkunal.image.ImageClassificationActivity;
+import com.example.mlkunal.image.ObjectDetectionActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onGotoFaceDetection(View view) {
+        // start the image helper activity
+        Intent intent = new Intent(this, FaceDetectionActivity.class);
+        startActivity(intent);
+    }
+
+
+}
---
+++ helpers/BoxWithLabel.java
@@ -0,0 +1,15 @@
+package com.example.mlkunal.helpers;
+
+import android.graphics.Rect;
+
+public class BoxWithLabel {
+
+    public Rect rect;
+    public String label;
+
+
+    public BoxWithLabel(Rect rect, String label) {
+        this.rect = rect;
+        this.label = label;
+    }
+}
